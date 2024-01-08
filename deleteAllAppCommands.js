3f4d72e0169d2fc1a03efe4d0c4a337080b6c352
hiroshimorowaka <guilhermecabral1204@gmail.com>
2024-01-08 15:03:55 -0300

    Creating new delete commands files for guild and global

---
+++ deleteAllAppCommands.js
@@ -0,0 +1,14 @@
+require('dotenv/config')
+
+const { REST, Routes } = require('discord.js');
+const { BOT_TOKEN, BOT_ID, GUILD_ID } = process.env
+
+const rest = new REST().setToken(BOT_TOKEN);
+
+// ...
+
+// for guild-based commands
+
+rest.put(Routes.applicationCommands(BOT_ID), { body: [] })
+	.then(() => console.log('Successfully deleted all global commands.'))
+	.catch(console.error);
