f55c64ed1eafc6305170a6caea7ca1342af7a12f
hiroshimorowaka <guilhermecabral1204@gmail.com>
2024-01-13 15:37:05 -0300

    Removing console log from `statusChange.js`

--- src/events/ready/statusChange.js
+++ src/events/ready/statusChange.js
@@ -2,13 +2,13 @@ const status = require("../../../status.json");
 
 module.exports = (client) => {
 	if (status.length > 1) {
-		console.log("more than 1");
+		// console.log("more than 1 activity");
 		setInterval(() => {
 			const random = Math.floor(Math.random() * status.length);
 			client.user.setActivity(status[random]);
 		}, 15 * 1000);
 	} else {
-		console.log("1 activity only");
+		// console.log("1 activity only");
 		client.user.setActivity(status[0]);
 	}
 };
