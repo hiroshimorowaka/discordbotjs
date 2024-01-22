cbc149115f12c6f24db0e0f7492fa064e04d067b
hiroshimorowaka <guilhermecabral1204@gmail.com>
2024-01-22 16:56:06 -0300

    Added locale to cooldown validation

--- src/validations/01commandCooldown.js
+++ src/validations/01commandCooldown.js
@@ -1,18 +1,29 @@
-const ms = require("ms-prettify").default;
+const { checkGuildLocale } = require("../models/guilds/locale");
+
 /**
  * @param {import('commandkit').SlashCommandProps} param0
  */
 
-module.exports = ({ interaction, commandObj }) => {
+module.exports = async ({ interaction, commandObj }) => {
 	const cooldown = interaction.client.cooldowns.get(`${interaction.user.id}_${commandObj.data.name}` || 0);
 	const date_now = Date.now();
+	const serverLocale = await checkGuildLocale(interaction.guildId);
+
+	const locales = {
+		"pt-BR": `Esse comando está em cooldown, ele ficará disponivel <t:${Math.round(
+			cooldown / 1000,
+		)}:R>, por favor aguarde!`,
+		"en-US": `This command is on cooldown, it will become available <t:${Math.round(
+			cooldown / 1000,
+		)}:R>, please wait!`,
+	};
+
 	if (date_now - cooldown < 0) {
 		interaction.reply({
-			content: `This command is on cooldown of <t:${Math.round(cooldown / 1000)}:R> ${ms(
-				cooldown - date_now,
-			)}, please wait!`,
+			content: locales[serverLocale] || locales["en-US"],
 			ephemeral: true,
 		});
+
 		return true;
 	}
 };
