c6ef585db9fd63846908f33ca431e892a42fbb9c
hiroshimorowaka <guilhermecabral1204@gmail.com>
2024-01-22 22:02:01 -0300

    Status

--- src/events/ready/statusChange.js
+++ src/events/ready/statusChange.js
@@ -1,24 +1,12 @@
 const status = require("../../../status.json");
-const { Client } = require("discord.js");
-/**
- *
- * @param {Client} client
- */
 
-module.exports = async (client) => {
-	let guildCount = client.guilds.cache.get() || (await client.guilds.fetch());
-
-	setInterval(async () => {
-		guildCount = client.guilds.cache.get() || (await client.guilds.fetch());
-	}, 60000);
-
+module.exports = (client) => {
 	if (status.length > 1) {
 		setInterval(() => {
 			const random = Math.floor(Math.random() * status.length);
-
-			client.user.setActivity(status[random].replace("{guildcount}", guildCount));
+			client.user.setActivity(status[random]);
 		}, 15 * 1000);
 	} else {
-		client.user.setActivity(status[0].replace("{guildcount}", guildCount));
+		client.user.setActivity(status[0]);
 	}
 };
