527a591a016b728d6994715888198e1a60476e71
hiroshimorowaka <guilhermecabral1204@gmail.com>
2024-01-24 04:31:54 -0300

    Improving timestamp output

--- logger.js
+++ logger.js
@@ -1,24 +1,32 @@
 const { format } = require("date-fns");
 
+const dateFormat = new Intl.DateTimeFormat("pt-BR", {
+	timeZone: "America/Sao_Paulo",
+	month: "2-digit",
+	day: "2-digit",
+	hour: "2-digit",
+	minute: "2-digit",
+	second: "2-digit",
+	timeZoneName: "shortOffset",
+});
 const pino = require("pino")({
-  level: "trace",
-  transport: {
-    targets: [
-      {
-        target: "pino-pretty",
-        options: {
-          colorize: true,
-        },
-      },
-      {
-        target: "pino/file",
-        options: { destination: `${__dirname}/app.log` },
-      },
-    ],
-  },
-
-  timestamp: () =>
-    `,"time":"${format(new Date(Date.now()), "dd/MM H:mm:ss.SS")}"`,
+	level: "debug",
+	transport: {
+		targets: [
+			{
+				target: "pino-pretty",
+				options: {
+					colorize: true,
+				},
+			},
+			{
+				target: "pino/file",
+				options: { destination: `${__dirname}/app.log` },
+			},
+		],
+	},
+	timestamp: () => `,"time":"${dateFormat.format(new Date(Date.now()))}"`,
+	// timestamp: () => `,"time":"${format(new Date(Date.now()), "dd/MM H:mm:ss.SS")}"`,
 });
 
 module.exports = pino;
